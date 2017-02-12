a1c8cb3be55522ae989d741a380cd563c80452f5
Sébastien Rouault <srlt@users.noreply.github.com>
2017-02-12 16:10:32 +0100

    Added 'std::bind' test and calls trace in the functor class.

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -24,6 +24,7 @@
 // ▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
 
 // External headers
+#include <functional>
 #include <iostream>
 
 // Internal headers
@@ -54,12 +55,22 @@ static void test_standalone() {
         Function<float(float)> funcA = standalone;
         Function<float(float)> funcB = funcA;
         ::std::cout << "- [copy] standalone -> standalone: " << funcA(3) << ", " << funcB(3) << ::std::endl;
+        if (funcA) {
+            ::std::cout << "- source still valid" << ::std::endl;
+        } else {
+            ::std::cout << "- source not valid anymore" << ::std::endl;
+        }
     }
     { // Move
         Function<float(float), 64> funcA = standalone;
         auto r = funcA(3);
         Function<float(float), 64> funcB = ::std::move(funcA);
         ::std::cout << "- [move] standalone -> standalone: " << r << ", " << funcB(3) << ::std::endl;
+        if (funcA) {
+            ::std::cout << "- source still valid" << ::std::endl;
+        } else {
+            ::std::cout << "- source not valid anymore" << ::std::endl;
+        }
     }
 }
 
@@ -122,6 +133,63 @@ static void test_lambda() {
 
 // ―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
 
+/** Bind (= functor) manipulation.
+**/
+static void test_bind() {
+    float a = 1;
+    auto lambda = [&](float x, float b) -> float {
+        return a * x + b;
+    };
+    auto bind = ::std::bind(lambda, ::std::placeholders::_1, 2);
+    ::std::cout << "Bind functor:" << ::std::endl;
+    { // Copy from local to local
+        Function<float(float), 64> funcA = bind;
+        Function<float(float), 64> funcB = funcA;
+        ::std::cout << "- [copy] local -> local: " << funcA(3) << ", " << funcB(3) << ::std::endl;
+    }
+    { // Copy from local to remote
+        Function<float(float), 64> funcA = bind;
+        Function<float(float), 0> funcB = funcA;
+        ::std::cout << "- [copy] local -> remote: " << funcA(3) << ", " << funcB(3) << ::std::endl;
+    }
+    { // Copy from remote to local
+        Function<float(float), 0> funcA = bind;
+        Function<float(float), 64> funcB = funcA;
+        ::std::cout << "- [copy] remote -> local: " << funcA(3) << ", " << funcB(3) << ::std::endl;
+    }
+    { // Copy from remote to remote
+        Function<float(float), 0> funcA = bind;
+        Function<float(float), 0> funcB = funcA;
+        ::std::cout << "- [copy] remote -> remote: " << funcA(3) << ", " << funcB(3) << ::std::endl;
+    }
+    { // Move from local to local
+        Function<float(float), 64> funcA = bind;
+        auto r = funcA(3);
+        Function<float(float), 64> funcB = ::std::move(funcA);
+        ::std::cout << "- [move] local -> local: " << r << ", " << funcB(3) << ::std::endl;
+    }
+    { // Move from local to remote
+        Function<float(float), 64> funcA = bind;
+        auto r = funcA(3);
+        Function<float(float), 0> funcB = ::std::move(funcA);
+        ::std::cout << "- [move] local -> remote: " << r << ", " << funcB(3) << ::std::endl;
+    }
+    { // Move from remote to local
+        Function<float(float), 0> funcA = bind;
+        auto r = funcA(3);
+        Function<float(float), 64> funcB = ::std::move(funcA);
+        ::std::cout << "- [move] remote -> local: " << r << ", " << funcB(3) << ::std::endl;
+    }
+    { // Move from remote to remote
+        Function<float(float), 0> funcA = bind;
+        auto r = funcA(3);
+        Function<float(float), 0> funcB = ::std::move(funcA);
+        ::std::cout << "- [move] remote -> remote: " << r << ", " << funcB(3) << ::std::endl;
+    }
+}
+
+// ―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
+
 /** Other functor manipulation.
 **/
 static void test_functor() {
@@ -134,7 +202,40 @@ static void test_functor() {
     public:
         /** Value constructor.
         **/
-        Test(float a, float b): a(a), b(b) {}
+        Test(float a, float b): a(a), b(b) {
+            ::std::cout << "  - trace: value constructing " << this << ::std::endl;
+        }
+        /** Copy constructor/assignment.
+         * @param test Instance to copy
+         * @return Current instance
+        **/
+        Test(Test const& test): a(test.a), b(test.b) {
+            ::std::cout << "  - trace: copy constructing " << this << " with " << ::std::addressof(test) << ::std::endl;
+        }
+        Test& operator=(Test const& test) {
+            a = test.a;
+            b = test.b;
+            ::std::cout << "  - trace: copy assigning " << this << " with " << ::std::addressof(test) << ::std::endl;
+            return *this;
+        }
+        /** Move constructor/assignment.
+         * @param test Instance to move
+         * @return Current instance
+        **/
+        Test(Test&& test): a(test.a), b(test.b) {
+            ::std::cout << "  - trace: move constructing " << this << " with " << ::std::addressof(test) << ::std::endl;
+        }
+        Test& operator=(Test&& test) {
+            a = test.a;
+            b = test.b;
+            ::std::cout << "  - trace: move assigning " << this << " with " << ::std::addressof(test) << ::std::endl;
+            return *this;
+        }
+        /** Destructor.
+        **/
+        ~Test() {
+            ::std::cout << "  - trace: destroying " << this << ::std::endl;
+        }
     public:
         /** Computes y = a * x + b.
          * @param x
@@ -144,47 +245,48 @@ static void test_functor() {
             return a * x + b;
         }
     };
+    auto test = Test(1, 2);
     ::std::cout << "Other functor:" << ::std::endl;
     { // Copy from local to local
-        Function<float(float), 64> funcA = Test(1, 2);
+        Function<float(float), 64> funcA = test;
         Function<float(float), 64> funcB = funcA;
         ::std::cout << "- [copy] local -> local: " << funcA(3) << ", " << funcB(3) << ::std::endl;
     }
     { // Copy from local to remote
-        Function<float(float), 64> funcA = Test(1, 2);
+        Function<float(float), 64> funcA = test;
         Function<float(float), 0> funcB = funcA;
         ::std::cout << "- [copy] local -> remote: " << funcA(3) << ", " << funcB(3) << ::std::endl;
     }
     { // Copy from remote to local
-        Function<float(float), 0> funcA = Test(1, 2);
+        Function<float(float), 0> funcA = test;
         Function<float(float), 64> funcB = funcA;
         ::std::cout << "- [copy] remote -> local: " << funcA(3) << ", " << funcB(3) << ::std::endl;
     }
     { // Copy from remote to remote
-        Function<float(float), 0> funcA = Test(1, 2);
+        Function<float(float), 0> funcA = test;
         Function<float(float), 0> funcB = funcA;
         ::std::cout << "- [copy] remote -> remote: " << funcA(3) << ", " << funcB(3) << ::std::endl;
     }
     { // Move from local to local
-        Function<float(float), 64> funcA = Test(1, 2);
+        Function<float(float), 64> funcA = test;
         auto r = funcA(3);
         Function<float(float), 64> funcB = ::std::move(funcA);
         ::std::cout << "- [move] local -> local: " << r << ", " << funcB(3) << ::std::endl;
     }
     { // Move from local to remote
-        Function<float(float), 64> funcA = Test(1, 2);
+        Function<float(float), 64> funcA = test;
         auto r = funcA(3);
         Function<float(float), 0> funcB = ::std::move(funcA);
         ::std::cout << "- [move] local -> remote: " << r << ", " << funcB(3) << ::std::endl;
     }
     { // Move from remote to local
-        Function<float(float), 0> funcA = Test(1, 2);
+        Function<float(float), 0> funcA = test;
         auto r = funcA(3);
         Function<float(float), 64> funcB = ::std::move(funcA);
         ::std::cout << "- [move] remote -> local: " << r << ", " << funcB(3) << ::std::endl;
     }
     { // Move from remote to remote
-        Function<float(float), 0> funcA = Test(1, 2);
+        Function<float(float), 0> funcA = test;
         auto r = funcA(3);
         Function<float(float), 0> funcB = ::std::move(funcA);
         ::std::cout << "- [move] remote -> remote: " << r << ", " << funcB(3) << ::std::endl;
@@ -205,6 +307,7 @@ int main(int argc, char** argv) {
     try {
         test_standalone();
         test_lambda();
+        test_bind();
         test_functor();
     } catch (Exception::Any const& err) {
         ::std::cout << err.what() << ::std::endl;
